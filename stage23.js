bfefc6d8233023cc6be518fdd8c36be081f90641
hphmhc <harishphchheenah@gmail.com>
2026-07-10 15:38:54 +0500

    Implement Stage 23 client financial summary

--- stage23.js
+++ stage23.js
@@ -1,1 +1,1 @@
-console.log('Stage 23 script loaded');
+(function(){const ST='Development Stage 23',$=s=>document.querySelector(s),A=s=>Array.from(document.querySelectorAll(s)),fmt=v=>'Rs. '+Number(v||0).toLocaleString('en-PK'),esc=v=>String(v??'').replace(/[&<>'"]/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;',"'":'&#39;','"':'&quot;'}[c]));function ready(){return typeof supabaseClient!='undefined'&&typeof state!='undefined'}function cname(id){return typeof getClientName=='function
